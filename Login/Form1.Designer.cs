1bc485e7648e297b822afca3cafce9310b5d845a
Fernando <eze980523@gmail.com>
2023-10-14 21:11:49 -0300

    Creación del diseño del formulario de login

--- Login/Form1.Designer.cs
+++ Login/Form1.Designer.cs
@@ -20,7 +20,7 @@ namespace Login
                 components.Dispose();
             }
             base.Dispose(disposing);
-           
+
         }
 
         #region Windows Form Designer generated code
@@ -31,12 +31,89 @@ namespace Login
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            btnIngresar = new Button();
+            btnCancelar = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(25, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(97, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Ingrese su correo";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(25, 75);
+            label2.Name = "label2";
+            label2.Size = new Size(116, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Ingrese su contraeña";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(25, 38);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(113, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(25, 93);
+            textBox2.Name = "textBox2";
+            textBox2.PasswordChar = '*';
+            textBox2.Size = new Size(113, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // btnIngresar
+            // 
+            btnIngresar.Location = new Point(25, 132);
+            btnIngresar.Name = "btnIngresar";
+            btnIngresar.Size = new Size(113, 23);
+            btnIngresar.TabIndex = 4;
+            btnIngresar.Text = "Ingresar";
+            btnIngresar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.Location = new Point(25, 161);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(113, 23);
+            btnCancelar.TabIndex = 5;
+            btnCancelar.Text = "Cancelar";
+            btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(173, 218);
+            Controls.Add(btnCancelar);
+            Controls.Add(btnIngresar);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button btnIngresar;
+        private Button btnCancelar;
     }
 }
